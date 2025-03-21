2a74a1078fd1c87ea0e2527e6f232a43f6ab7f27
MuhammadAffan-06 <maffan2202@gmail.com>
2025-03-21 05:30:22 +0500

    minor deletions made

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+
 import Navbar from "@/app/components/navbar/navbar";
 import Translation from "@/app/components/translation/translation";
 import "./globals.css";
